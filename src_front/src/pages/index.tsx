94b5f4df1718dba10cfec52b21cd665728dcba96
Alexander Sokolov <gunpowderfans@mail.ru>
2020-11-29 05:00:19 +0800

    Able to list files

--- src_front/src/pages/index.tsx
+++ src_front/src/pages/index.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import Layout from "@/layouts/index"
 import {List} from "antd-mobile";
-import {IDirectoryState, useSelector} from 'umi'
+import {FolderOutlined, PictureOutlined, RollbackOutlined} from "@ant-design/icons";
+import {IDirectoryState, useDispatch, useSelector} from 'umi'
 
 export default () => {
+	const dispatch = useDispatch()
 	const directory: IDirectoryState = useSelector((state: any) => state.directory)
+	const handleClick = (isDirectory: boolean, name: string) => {
+		if (isDirectory) {
+			return () => {
+				dispatch({type: "directory/changeDir", to: name})
+			}
+		}
+		return () => {
+		}
+	}
 	return (
 		<Layout>
+			{/*{JSON.stringify(directory)}*/}
 			<List>
-				<List.Item>A</List.Item>
+				{directory.pwd === "/" ? null :
+					<List.Item onClick={handleClick(true, "..")}><RollbackOutlined/>{" "}..</List.Item>}
+				{directory.files.map(item =>
+					<List.Item onClick={handleClick(item.isDirectory, item.name)}>
+						{item.isDirectory ? <FolderOutlined/> : <PictureOutlined/>}
+						{" "}
+						{item.name}
+					</List.Item>
+				)}
 			</List>
 		</Layout>
 	);
