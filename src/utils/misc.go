95700fac4ab35a89a3f952a4c34bd79d4045f8a6
Alexander Sokolov <gunpowderfans@mail.ru>
2020-11-29 04:49:06 +0800

    Arrange IsPicture to check.go Rename ListDo to ListDir

--- src/utils/misc.go
+++ src/utils/misc.go
@@ -5,14 +5,8 @@ import (
 	"path/filepath"
 )
 
-// 判断文件扩展名是不是图片文件的
-func IsPicture(filename string) bool {
-	ext := filepath.Ext(filename)
-	return ext == ".jpg" || ext == ".jpeg" || ext == ".jpe" || ext == ".png" || ext == ".tif" || ext == ".tiff" || ext == ".bmp" || ext == ".svg" || ext == ".gif"
-}
-
 // 参考filepath.Walk，实现了一个仅遍历一层的版本
-func ListDo(dirname string, listFn filepath.WalkFunc) error {
+func ListDir(dirname string, listFn filepath.WalkFunc) error {
 	list, err := ioutil.ReadDir(dirname)
 	if err != nil {
 		return err
