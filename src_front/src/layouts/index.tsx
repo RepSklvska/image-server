d3bd561db2754c1cbd5eafd8e5f664df0315ae93
Alexander Sokolov <gunpowderfans@mail.ru>
2020-11-29 04:51:54 +0800

    NavBar shows basename instead

--- src_front/src/layouts/index.tsx
+++ src_front/src/layouts/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import {Icon, NavBar} from "antd-mobile"
+import {useSelector} from "umi";
 
 const NavTop: React.FC = () => {
-
+	const basename: string = useSelector((state: any) => {
+		const pwd: string = state.directory.pwd
+		if (pwd === "/") {
+			return "/"
+		}
+		const pwdSplited = pwd.split("/")
+		return pwdSplited[pwdSplited.length - 1]
+	})
 	return (
 		<NavBar
 			mode="dark"
 			icon={<Icon type="left"/>}
 		>
-			Image Server
+			{basename}
 		</NavBar>
 	)
 }
