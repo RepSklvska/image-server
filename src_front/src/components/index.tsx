6823493ebd4f6d053aca86b47708935e98e4fafc
Alexander Sokolov <gunpowderfans@mail.ru>
2020-12-16 09:01:25 +0800

    Can view picture now

--- src_front/src/components/index.tsx
+++ src_front/src/components/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {CloseCircleTwoTone} from "@ant-design/icons";
 import {Flex} from "antd-mobile";
 import {useSelector} from "@@/plugin-dva/exports";
@@ -24,8 +24,20 @@ const CloseButton: React.FC<ICloseButton> = ({onClick}) => {
 	)
 }
 
-const BlobToSrc = (blob: Blob) => {
+const BlobToSrc = async (blob: Blob) => {
+	try {
+		const buffer = await blob.arrayBuffer()
+		// const buffer = await (new Response(blob)).arrayBuffer()
+		console.log(blob)
+		let text = `data:${blob.type};base64,`
+		console.log(text)
 
+		text += btoa(String.fromCharCode(...new Uint8Array(buffer)))
+
+		return text
+	} catch (e) {
+		console.log(e)
+	}
 }
 
 interface IPictureViewer {
@@ -41,18 +53,21 @@ const PictureViewer: React.FC<IPictureViewer> = ({pictures}) => {
 	const pictureBlob: Blob = useSelector((state: any) => state.picture.picture1)
 	pictureBlob
 	const dispatch = useDispatch()
+
+	const [imgsrc, setImgsrc] = useState<string>("114514")
+	BlobToSrc(pictureBlob).then(result => {
+		if (result) setImgsrc(result)
+	})
+
 	return (
 		<div style={{
 			width: "100%", height: "100%", background: "rgba(0,0,0,.75)",
-			position: "absolute", zIndex: 10, top: 0, left: 0
+			position: "fixed", zIndex: 10, top: 0, left: 0
 		}} hidden={hidden}>
 			<CloseButton onClick={() => dispatch({type: "picture/handleCloseViewer"})}/>
 			<Flex direction="column" justify="center" style={{height: "100%"}}>
 				<Flex justify="center">
-					{/*<div style={{color: "white"}}>*/}
-					{/*	{pictureString}*/}
-					{/*</div>*/}
-					<img alt="" src=""/>
+					<img alt="" src={imgsrc} style={{maxHeight:"100vh",maxWidth:"100vw"}}/>
 				</Flex>
 			</Flex>
 		</div>
